603114db158212a362b2725b48ba317d103d00d9
mkiw1464-debug <mkiw1464@gmail.com>
2026-02-16 14:28:21 +0800

    Update KeyAuth.mm

--- KeyAuth.mm
+++ KeyAuth.mm
@@ -8,10 +8,7 @@ std::string getHWID() {
     return std::string([[[UIDevice currentDevice] identifierForVendor].UUIDString UTF8String]);
 }
 
-std::string performPOST(const std::string& postData) {
-    __block std::string result;
-    dispatch_semaphore_t sem = dispatch_semaphore_create(0);
-    
+void performPOST(const std::string& postData, void(^completion)(std::string resp)) {
     NSURL *url = [NSURL URLWithString:@"https://keyauth.win/api/1.2/"];
     NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
     [req setHTTPMethod:@"POST"];
@@ -19,24 +16,13 @@ std::string performPOST(const std::string& postData) {
     [req setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
     
     NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:req completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        std::string result = "";
         if (data) result = std::string((char*)[data bytes], [data length]);
-        dispatch_semaphore_signal(sem);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            completion(result);
+        });
     }];
     [task resume];
-    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
-    return result;
-}
-
-std::string extractMessage(const std::string& resp) {
-    size_t pos = resp.find("\"message\":\"");
-    if (pos != std::string::npos) {
-        pos += 11;
-        size_t end = resp.find("\"", pos);
-        if (end != std::string::npos) {
-            return resp.substr(pos, end - pos);
-        }
-    }
-    return "Unknown error from KeyAuth";
 }
 
 class KeyAuth {
@@ -48,97 +34,107 @@ public:
     std::string sessionid;
     std::string lastError;
 
-    bool init() {
+    void init(void(^callback)(bool success)) {
         std::string post = "type=init&ver=" + version + "&name=" + name + "&ownerid=" + ownerid + "&secret=" + secret;
-        std::string resp = performPOST(post);
-        if (resp.find("\"success\":true") != std::string::npos) {
-            size_t pos = resp.find("\"sessionid\":\"");
-            if (pos != std::string::npos) {
-                pos += 14;
-                size_t end = resp.find("\"", pos);
-                sessionid = resp.substr(pos, end - pos);
+        performPOST(post, ^(std::string resp) {
+            if (resp.find("\"success\":true") != std::string::npos) {
+                size_t pos = resp.find("\"sessionid\":\"");
+                if (pos != std::string::npos) {
+                    pos += 14;
+                    size_t end = resp.find("\"", pos);
+                    sessionid = resp.substr(pos, end - pos);
+                }
+                callback(true);
+            } else {
+                lastError = "Init failed";
+                callback(false);
             }
-            return true;
-        }
-        return false;
+        });
     }
 
-    bool checkKey(const std::string& key) {
-        if (sessionid.empty() && !init()) {
-            lastError = "Failed to init session";
-            return false;
+    void checkKey(const std::string& key, void(^callback)(bool valid, std::string error)) {
+        if (sessionid.empty()) {
+            init(^(bool ok) {
+                if (ok) checkKey(key, callback);
+                else callback(false, "Failed to connect to server");
+            });
+            return;
         }
-        
-        std::string post = "type=license&key=" + key +
-                           "&sessionid=" + sessionid +
-                           "&name=" + name +
-                           "&ownerid=" + ownerid +
-                           "&secret=" + secret +
+
+        std::string post = "type=license&key=" + key + "&sessionid=" + sessionid +
+                           "&name=" + name + "&ownerid=" + ownerid + "&secret=" + secret +
                            "&hwid=" + getHWID();
-        
-        std::string resp = performPOST(post);
-        if (resp.find("\"success\":true") != std::string::npos) {
-            return true;
-        } else {
-            lastError = extractMessage(resp);
-            NSLog(@"KeyAuth Debug: %s", resp.c_str()); // untuk log kalau ada
-            return false;
-        }
+
+        performPOST(post, ^(std::string resp) {
+            if (resp.find("\"success\":true") != std::string::npos) {
+                callback(true, "");
+            } else {
+                size_t pos = resp.find("\"message\":\"");
+                std::string msg = "Invalid key";
+                if (pos != std::string::npos) {
+                    pos += 11;
+                    size_t end = resp.find("\"", pos);
+                    if (end != std::string::npos) msg = resp.substr(pos, end - pos);
+                }
+                callback(false, msg);
+            }
+        });
     }
 };
 
-// Keychain functions (sama macam sebelum ni)
-std::string loadKeyFromKeychain() { /* sama */ }
-bool saveKeyToKeychain(const std::string& key) { /* sama */ }
-void deleteKeyFromKeychain() { /* sama */ }
+// Keychain (sama)
+std::string loadKeyFromKeychain() { /* copy dari code lama */ }
+bool saveKeyToKeychain(const std::string& key) { /* copy dari code lama */ }
+void deleteKeyFromKeychain() { /* copy dari code lama */ }
 
-// GUI dengan error message tepat
+// GUI
 void showKeyPrompt(const std::string& errorMsg = "") {
     dispatch_async(dispatch_get_main_queue(), ^{
-        NSString *title = errorMsg.empty() ? @"Azurite KeyAuth" : @"Key Tidak Sah";
-        NSString *message = errorMsg.empty() ? @"Masukkan license key anda" : [NSString stringWithUTF8String:errorMsg.c_str()];
-        
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:title
-                                                                       message:message
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:errorMsg.empty() ? @"Azurite KeyAuth" : @"Key Tidak Sah"
+                                                                       message:errorMsg.empty() ? @"Masukkan license key" : [NSString stringWithUTF8String:errorMsg.c_str()]
                                                                 preferredStyle:UIAlertControllerStyleAlert];
         
-        [alert addTextFieldWithConfigurationHandler:^(UITextField *tf) {
-            tf.placeholder = @"Paste key sini";
+        [alert addTextFieldWithConfigurationHandler:^(UITextField *tf) { tf.placeholder = @"Paste key sini"; }];
+        
+        UIAlertAction *submit = [UIAlertAction actionWithTitle:@"Submit" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
+            std::string k = [[[alert.textFields[0] text] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] UTF8String];
+            if (k.empty()) { showKeyPrompt(); return; }
+            
+            // Loading
+            UIAlertController *loading = [UIAlertController alertControllerWithTitle:@"Checking Key..." message:nil preferredStyle:UIAlertControllerStyleAlert];
+            [topViewController() presentViewController:loading animated:YES completion:nil];
+            
+            KeyAuth().checkKey(k, ^(bool valid, std::string err) {
+                [loading dismissViewControllerAnimated:YES completion:^{
+                    if (valid) {
+                        saveKeyToKeychain(k);
+                    } else {
+                        showKeyPrompt(err);
+                    }
+                }];
+            });
         }];
         
-        [alert addAction:[UIAlertAction actionWithTitle:@"Submit" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-            UITextField *tf = alert.textFields.firstObject;
-            std::string k = [[[tf.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] stringByReplacingOccurrencesOfString:@" " withString:@""] UTF8String];
-            
-            if (k.empty()) {
-                showKeyPrompt();
-                return;
-            }
-            
-            KeyAuth auth;
-            if (auth.checkKey(k)) {
-                saveKeyToKeychain(k);
-                // Key OK
-            } else {
-                showKeyPrompt(auth.lastError);
-            }
-        }]];
-        
+        [alert addAction:submit];
         [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *) { exit(0); }]];
         
-        UIViewController *vc = [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentedViewController] ?: [[[UIApplication sharedApplication] keyWindow] rootViewController];
-        [vc presentViewController:alert animated:YES completion:nil];
+        [topViewController() presentViewController:alert animated:YES completion:nil];
     });
 }
 
 // Auto start
-__attribute__((constructor)) static void init() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+__attribute__((constructor))
+static void initKeyAuth() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 6 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         std::string saved = loadKeyFromKeychain();
         if (!saved.empty()) {
             KeyAuth auth;
-            if (auth.checkKey(saved)) return;
-            deleteKeyFromKeychain();
+            auth.checkKey(saved, ^(bool valid, std::string err) {
+                if (valid) return;
+                deleteKeyFromKeychain();
+                showKeyPrompt(err);
+            });
+            return;
         }
         showKeyPrompt();
     });
